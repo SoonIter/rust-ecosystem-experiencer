553d40e6b1eaea5656bf45710ac78fa36b6bf909
SoonIter <sooniter@gmail.com>
2023-05-18 02:39:26 +0800

    feat: add indicatif_demo

---
+++ cli_crates/indicatif_demo/examples/hello_world.rs
@@ -0,0 +1,18 @@
+use std::{
+  thread,
+  time::Duration,
+};
+
+use indicatif::ProgressBar;
+
+fn main() {
+  let deps = 10;
+  let pb = ProgressBar::new(deps);
+
+  for _ in 0..deps {
+    thread::sleep(Duration::from_millis(100));
+    pb.inc(1);
+  }
+
+  pb.finish_and_clear();
+}
---
+++ crates/rayon_demo/src/main.rs
@@ -0,0 +1,28 @@
+use rayon::prelude::*;
+use std::time::Instant;
+
+fn count(i: f64, n: f64) -> f64 {
+    (4.0 / (1.0 + ((i + 0.5) / n) * ((i + 0.5) / n))) / n
+}
+
+// Calculus get PI
+fn main() {
+    let start = Instant::now();
+
+    let n = 100000000;
+    let n_f64 = 100000000.0;
+
+    let res: f64 = (0..n)
+        .into_par_iter()
+        .map(|i| {
+            count(i as f64, n_f64)
+        }).sum::<f64>();
+
+    dbg!(res);
+    let duration = start.elapsed();
+    println!("Time elapsed in expensive_function() is: {:?}", duration);
+    /*
+     100000 1.1865ms
+     100000000 312.462208ms
+    */
+}
